5b9e66449fc22065cbf8e6aeb4a302a8633df40c
Tallys <tallys.prado@gmail.com>
2021-04-02 15:40:16 -0300

    add error trat

--- heapsort.py
+++ heapsort.py
@@ -1,3 +1,5 @@
+import sys
+
 def heap(arr, n, i):
     largest = i  # raiz
     l = 2 * i + 1
@@ -21,14 +23,26 @@ def heapSort(arr):
         heap(arr, i, 0)
 
 
-file = open("entrada.data", "r")
-array = file.read().splitlines()
-arrayNumber = list(map(int, array))
-print(arrayNumber)
-heapSort(arrayNumber)
-n = len(arrayNumber)
-print("Array ordenado por HeapSort:")
-for i in range(n):
-    print("%d" %arrayNumber[i])
 
-file.close()
+
+try:
+    file = open(sys.argv[1], "r")
+    array = file.read().splitlines()
+except IndexError:
+    print("Você entrou com o arquivo entrada.data errado!!!\nVeja as informações abaixo:\n")
+except NameError: 
+    print("Você esqueceu de selecionar o entrada.data!!!\nVeja as informações abaixo:\n")
+except FileNotFoundError:
+    print("Você digitou o arquivo errado!!!!\nVeja as informações abaixo:\n")
+finally:
+    arrayNumber = list(map(int, array))
+    print(arrayNumber)
+    heapSort(arrayNumber)
+    n = len(arrayNumber)
+    print("Array ordenado por HeapSort:")
+    for i in range(n):
+        print("%d" %arrayNumber[i])
+    file.close()
+    
+
+
